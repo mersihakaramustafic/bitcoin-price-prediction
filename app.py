847a913acf29c3698ca098b3a03db1a346e75078
mersihakaramustafic <karamustafic.mersiha@gmail.com>
2025-01-07 18:02:12 +0200

    Importing ARIMA for predicting Bitcoin prices

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 import numpy as np
 import constants as c
+from statsmodels.tsa.arima.model import ARIMA
 
 def fetch_bitcoin_data_one_year():
 
